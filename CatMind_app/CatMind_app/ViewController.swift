bb521b0e78ceeecd2b0a11dce528a989ddea3bda
sato <sk117fuku@gmail.com>
2023-11-07 23:59:43 +0900

    first commit

---
+++ CatMind_app/CatMind_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatMind_app
+//
+//  Created by 佐藤今日香 on 2023/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CatMind_app/CatMind_appUITests/CatMind_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CatMind_appUITestsLaunchTests.swift
+//  CatMind_appUITests
+//
+//  Created by 佐藤今日香 on 2023/11/07.
+//
+
+import XCTest
+
+final class CatMind_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
